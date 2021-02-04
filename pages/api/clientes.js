3b43dfe33738c10f2ded5ffd5ca204571fd71c4c
Willy Christian <crisaort@gmail.com>
2021-02-04 14:04:37 -0300

    Add route validation.

--- pages/api/clientes.js
+++ pages/api/clientes.js
@@ -1,16 +1,31 @@
 import {connectToDatabase} from '../../util/mongodb'
 
 const Clients = async (req, res) =>{ 
-    try {
+   if(req.method === "POST"){
+       const {
+        full_name: "",
+        cpf: "",
+        phone: "",
+        cep: "",
+        street: "",
+        neighbour: "",
+        city: ""
+       } = req.body
+       if(
+        !full_name ||
+        !cpf ||
+        !phone
+       ){
+         res.status(400).json({message: "Há campos requeridos em branco"})
+           return
+        }
         const body = JSON.parse(req.body)
         const { db } = await connectToDatabase()
-        const response =  await db.collection("clientes").insertOne({
-            ...body
-        })
+        const response =  await db.collection("clientes").insertOne({})
         const clientReturn = JSON.stringify(response.ops)
         res.send(clientReturn)
-    } catch(err){
+   }
         res.status(400).send(err.message)
-    }
+    
 }
 export default Clients
