9ed36d0a44dda3fbac0b738210601395fbbdf429
WillyChristian <crisaort@gmail.com>
2021-02-05 22:19:12 -0300

    Feture: finish search route (with 3 parameters)

--- pages/api/clientes/consulta.js
+++ pages/api/clientes/consulta.js
@@ -5,28 +5,63 @@ const Consulta = async (req, res) => {
 		//recebe os dados do front
 		const { full_name, cpf, phone } = req.body;
 
-		//     || !cpf || !phone){
-		//  res.status(400).json({message: "Preencha ao menos um campo de pesquisa"})
+		if (full_name) {
+			const { db } = await connectToDatabase();
+			const response = await db
+				.collection("clientes")
+				.find({ full_name })
+				.toArray();
+			if (response.length === 0) {
+				res.status(400).json({
+					message: "Não foi encontrado usuário com este nome",
+				});
+				return;
+			} else {
+				res.status(200).json(response);
+				return;
+			}
+		}
 
-		// }
-		// if(full_name){
-		//     const { db } = await connectToDatabase()
-		//     const response =  await db.collection("clientes").find(full_name)
-		//     res.status(200).send(response)
-		//     return
-		// }
-		// if(cpf){
-		//     const { db } = await connectToDatabase()
-		//     const response =  await db.collection("clientes").find(cpf)
-		//     res.status(200).send(response)
-		//     return
-		// }
-		//  if(phone){
-		//     const { db } = await connectToDatabase()
-		//     const response =  await db.collection("clientes").find(phone)
-		//     res.status(200).send(response)
-		//     return
-		// }
+		if (cpf) {
+			const { db } = await connectToDatabase();
+			const response = await db
+				.collection("clientes")
+				.find({ cpf })
+				.toArray();
+			if (response.length === 0) {
+				res.status(400).json({
+					message: "Não foi encontrado usuário com este CPF",
+				});
+				return;
+			} else {
+				res.status(200).json(response);
+				return;
+			}
+		}
+
+		if (phone) {
+			const { db } = await connectToDatabase();
+			const response = await db
+				.collection("clientes")
+				.find({ phone })
+				.toArray();
+			if (response.length === 0) {
+				res.status(400).json({
+					message: "Não foi encontrado usuário com este Telefone",
+				});
+				return;
+			} else {
+				res.status(200).json(response);
+				return;
+			}
+		}
+
+		if (!full_name || !cpf || !phone) {
+			res.status(400).json({
+				message: "Preencha ao menos um campo de pesquisa",
+			});
+			return;
+		}
 	} else {
 		res.status(500).send({ Erro: "Parâmetro de envio deve ser GET" });
 	}
