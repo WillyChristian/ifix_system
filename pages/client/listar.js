0f94e8bebacd269d0f66c88e6c4f7894f0f0e995
WillyChristian <crisaort@gmail.com>
2021-02-10 00:50:29 -0300

    feature: add front end search method for clients

--- pages/client/listar.js
+++ pages/client/listar.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import useSWR from "swr"; // não sei
+import axios from "axios";
 import { Grid, Button, TextField, makeStyles } from "@material-ui/core";
 
 // Estilos
@@ -17,50 +19,50 @@ const style = makeStyles({
 		width: "500px auto",
 		alignItems: "space-btween",
 	},
+	gridItem1: {
+		backgroundColor: "#949494",
+	},
+	gridItem2: {
+		backgroundColor: "#9a9a9a",
+	},
 });
+
+// COMPONENTE
 const Listar = () => {
-	const [list, setList] = useState({
-		cpf: "",
-	});
+	const [client, setClient] = useState([]);
 
-	const atualiza = (event) => {
-		const value = event.target.value;
-		const key = event.target.id;
-		setList((old) => ({
-			...old,
-			[key]: value,
-		}));
-		console.log(list);
-	};
-
-	const listClients = async () => {
-		if (!list.cpf || list.cpf === "") {
-			alert("Insira um número de CPF válido!");
-		} else {
-			const response = await fetch("api/clients/read", {
-				method: "GET",
-				body: list.cpf,
-			});
-			if (response.status === 200) {
-				alert();
-			} else {
-			}
-		}
+	const getClientLIst = async () => {
+		const cpf = document.getElementById("cpf").value;
+		const url = `api/clients/${cpf}`;
+		await axios.get(url).then((resp) => {
+			setClient(resp.data);
+		});
 	};
 
 	const listarStyle = style();
 	return (
 		<div className={listarStyle.container}>
 			<div className={listarStyle.header}>
-				<TextField
-					placeholder='Informe o CPF do cliente'
-					id='cpf'
-					onChange={atualiza}
-				/>
-				<Button onClick={listClients}>Listar</Button>
+				<TextField placeholder='Informe o CPF do cliente' id='cpf' />
+				<Button onClick={getClientLIst}>Listar</Button>
 			</div>
-			<Grid container>
-				<Grid item></Grid>
+			<Grid container spacing={2} alignContent={"center"} justify={"center"}>
+				{client.map((event) => {
+					return (
+						<>
+							<Grid item classes={{ root: listarStyle.gridItem1 }}>
+								<p>ID: {event._id}</p>
+								<p>Nome: {event.full_name}</p>
+								<p>CPF: {event.cpf}</p>
+								<p>Telefone: {event.phone}</p>
+								<p>CEP: {event.cep}</p>
+								<p>Rua: {event.street}</p>
+								<p>Bairro: {event.neighbour}</p>
+								<p>Cidade: {event.city}</p>
+							</Grid>
+						</>
+					);
+				})}
 			</Grid>
 		</div>
 	);
